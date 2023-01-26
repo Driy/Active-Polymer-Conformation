8154bdd5a0c34c5226dcebd55b8fbf4d5f38364b
Andriy Goychuk <andriy.goychuk@gmail.com>
2023-01-26 15:13:17 -0500

    Added method to numerically compute conformation
    in the case of time-delayed excitation pulses.

--- Julia/Source/ExcitationProgram/DeltaSequence/RealSpace.jl
+++ Julia/Source/ExcitationProgram/DeltaSequence/RealSpace.jl
@@ -8,6 +8,29 @@ using ....Methods
 using ....Methods.FastFourier
 
 """
+compute_conformation(delay_sequence, excitation_sequence, J::Function; fourier_type = FastFourier.DCT)
+
+Calculate the mean square separation of a polymer with given correlation matrix `matrix_activity` and Jacobian `J`.
+Use the argument `fourier_type` to specialize this function to specific transforms, such as the Discrete Cosine Transform or the Fast Fourier Transform.
+"""
+function compute_conformation(delay_sequence, excitation_sequence, J::Function; fourier_type = FastFourier.DCT)
+    # transform to Fourier space; always dense!
+    excitation_sequence_fourier = [FastFourier.forward(matrix_activity, fourier_type = fourier_type) 
+        for matrix_activity in excitation_sequence];
+    
+    # get effective excitation matrix
+    tmp = Spectral.effective_correlation(delay_sequence, excitation_sequence_fourier, J; fourier_type = fourier_type)
+
+    # manipulate as needed in Fourier space
+    Methods.Spectral.activity_to_correlation!(tmp, J, fourier_type = fourier_type);
+    
+    # transform to real space
+    FastFourier.backward!(tmp, fourier_type = fourier_type);
+    # return mean square separation
+    return tmp;
+end
+
+"""
 compute_pairwise_velocity_correlation(matrix_activity::AbstractMatrix; J::Function = Jacobian.Standard.J₀, fourier_type = FastFourier.DCT)
 
 Calculate the mean squared traveled distance after the time τ, for all loci along a polymer with given correlation matrix `matrix_activity` and Jacobian `J`.
